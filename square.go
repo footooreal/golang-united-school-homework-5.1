ff49be3010ad7e222ba9ffc950f6405e7a6b0fb8
footooreal <107985446+footooreal@users.noreply.github.com>
2022-07-07 18:57:33 +0300

    Add files via upload

---
+++ square.go
@@ -0,0 +1,25 @@
+package square
+
+type Point struct {
+	x, y int
+}
+
+type Square struct {
+	start Point
+	a     uint
+}
+
+func (s *Square) End() Point {
+	p1 := s.start.x + int(s.a)
+	p2 := s.start.y - int(s.a)
+
+	return Point{p1, p2}
+}
+
+func (s *Square) Area() uint {
+	return s.a * s.a
+}
+
+func (s *Square) Perimeter() uint {
+	return s.a * 4
+}
